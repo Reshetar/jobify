b5efef4bf67ba4630c9875e3a61ea098fa72d0aa
Yevhen Reshetar <reshetar@YevhenMacBook-3063.local>
2022-11-17 14:54:51 +0200

    did pages (add job, all jobs), did functionality, displayed show stats on stats page

--- client/src/context/reducers.js
+++ client/src/context/reducers.js
@@ -1,6 +1,7 @@
-import { 
-  CLEAR_ALERT, 
-  DISPLAY_ALERT, 
+import { TOO_MANY_REQUESTS } from "http-status-codes";
+import {
+  CLEAR_ALERT,
+  DISPLAY_ALERT,
   SETUP_USER_BEGIN,
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
@@ -8,99 +9,206 @@ import {
   LOGOUT_USER,
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
-  UPDATE_USER_ERROR 
-} from './actions';
+  UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
+  CLEAR_VALUE,
+  CREATE_JOB_BEGIN,
+  CREATE_JOB_SUCCESS,
+  CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
+} from "./actions";
 
-import { initialState } from './appContext';
+import { initialState } from "./appContext";
 
 const reducer = (state, action) => {
   if (action.type === DISPLAY_ALERT) {
     return {
-      ...state, 
-      showAlert: true, 
-      alertType: 'danger', 
-      alertText: 'Please provide all values!',
-    }
+      ...state,
+      showAlert: true,
+      alertType: "danger",
+      alertText: "Please provide all values!",
+    };
   }
   if (action.type === CLEAR_ALERT) {
     return {
-      ...state, 
-      showAlert: false, 
-      alertType: '', 
-      alertText: '',
-    }
+      ...state,
+      showAlert: false,
+      alertType: "",
+      alertText: "",
+    };
   }
-  
+
   if (action.type === SETUP_USER_BEGIN) {
-    return {...state, isLoading: true};
+    return { ...state, isLoading: true };
   }
   if (action.type === SETUP_USER_SUCCESS) {
     return {
-      ...state, 
+      ...state,
       isLoading: false,
       token: action.payload.token,
       user: action.payload.user,
       userLocation: action.payload.location,
       jobLocation: action.payload.location,
-      showAlert: true, 
-      alertType: 'success', 
+      showAlert: true,
+      alertType: "success",
       alertText: action.payload.alertText,
     };
   }
   if (action.type === SETUP_USER_ERROR) {
     return {
-      ...state, 
+      ...state,
       isLoading: false,
-      showAlert: true, 
-      alertType: 'danger', 
+      showAlert: true,
+      alertType: "danger",
       alertText: action.payload.msg,
     };
   }
-  
+
   if (action.type === TOGGLE_SIDEBAR) {
     return {
-      ...state, 
+      ...state,
       showSidebar: !state.showSidebar,
     };
-  };
-  
+  }
+
   if (action.type === LOGOUT_USER) {
     return {
-      ...initialState, 
+      ...initialState,
       token: null,
       user: null,
-      userLocation: '',
-      jobLocation: '',
+      userLocation: "",
+      jobLocation: "",
     };
-  };
+  }
 
   if (action.type === UPDATE_USER_BEGIN) {
-    return {...state, isLoading: true};
+    return { ...state, isLoading: true };
   }
   if (action.type === UPDATE_USER_SUCCESS) {
     return {
-      ...state, 
+      ...state,
       isLoading: false,
       token: action.payload.token,
       user: action.payload.user,
       userLocation: action.payload.location,
       jobLocation: action.payload.location,
-      showAlert: true, 
-      alertType: 'success', 
-      alertText: 'User Profile Updated!',
+      showAlert: true,
+      alertType: "success",
+      alertText: "User Profile Updated!",
     };
   }
   if (action.type === UPDATE_USER_ERROR) {
     return {
-      ...state, 
+      ...state,
       isLoading: false,
-      showAlert: true, 
-      alertType: 'danger', 
+      showAlert: true,
+      alertType: "danger",
       alertText: action.payload.msg,
     };
   }
 
-  throw new Error(`no such action : ${action.type}`)
-}
+  if (action.type === HANDLE_CHANGE) {
+    return {
+      ...state,
+      [action.payload.name]: action.payload.value,
+    };
+  }
+  if (action.type === CLEAR_VALUE) {
+    const initialState = {
+      isEditing: false,
+      editJobId: "",
+      position: "",
+      company: "",
+      jobLocation: state.user.location,
+      jobType: "full-time",
+      status: "pending",
+    };
+
+    return {
+      ...state,
+      ...initialState,
+    };
+  }
+
+  if (action.type === CREATE_JOB_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === CREATE_JOB_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "success",
+      alertText: "New Job Created",
+    };
+  }
+  if (action.type === CREATE_JOB_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: "danger",
+      alertText: action.payload.msg,
+    };
+  }
+
+  if (action.type === GET_JOBS_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+      showAlert: false,
+    };
+  }
+  if (action.type === GET_JOBS_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      jobs: action.payload.jobs,
+      totalJobs: action.payload.totalJobs,
+      numOfPages: action.payload.numOfPages,
+    };
+  }
+
+  if (action.type === SET_EDIT_JOB) {
+    const job = state.jobs.find((job) => job._id === action.payload.id);
+    const { _id, position, company, jobLocation, jobType, status } = job;
+
+    return {
+      ...state,
+      isEditing: true,
+      editJobId: _id,
+      position,
+      company,
+      jobLocation,
+      jobType,
+      status,
+    };
+  }
+
+  if (action.type === SHOW_STATS_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+      showAlert: false,
+    }
+  }
+  if (action.type === SHOW_STATS_SUCCESS) {
+    return {
+      ...state,
+      isLoading: false,
+      stats: action.payload.stats,
+      monthlyApplications: action.payload.monthlyApplications,
+    }
+  }
+
+  throw new Error(`no such action : ${action.type}`);
+};
 
 export default reducer;
--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,7 +1,108 @@
+import { FormRow, Alert, FormRowSelect } from "../../components";
+import { useAppContext } from "../../context/appContext";
+import Wrapper from "../../assets/wrappers/DashboardFormPage";
+
 const AddJob = () => {
+  const {
+    isLoading,
+    isEditing,
+    showAlert,
+    displayAlert,
+    position,
+    company,
+    jobLocation,
+    jobType,
+    jobTypeOptions,
+    status,
+    statusOptions,
+    handleChange,
+    clearValues,
+    createJob,
+    editJob
+  } = useAppContext();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!position || !company || !jobLocation) {
+      displayAlert();
+      return;
+    }
+    if (isEditing) {
+      editJob();
+      return;
+    }
+    createJob();
+  };
+
+  const handleJobInput = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    handleChange({ name, value });
+  };
+
   return (
-    <div>AddJob</div>
-  )
-}
+    <Wrapper>
+      <form className="form">
+        <h3>{isEditing ? "Edit job" : "Add job"}</h3>
+        {showAlert && <Alert />}
+        <div className="form-center">
+          <FormRow
+            type="text"
+            name="position"
+            value={position}
+            handleChange={handleJobInput}
+          />
+          <FormRow
+            type="text"
+            name="company"
+            value={company}
+            handleChange={handleJobInput}
+          />
+          <FormRow
+            type="text"
+            labelText="Job Location"
+            name="jobLocation"
+            value={jobLocation}
+            handleChange={handleJobInput}
+          />
+          <FormRowSelect
+            name="status"
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText="Job type"
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
+          <div className="btn-container">
+            <button
+              className="btn btn-block clear-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                clearValues();
+                console.log("clear");
+              }}
+            >
+              Clear
+            </button>
+            <button
+              type="submit"
+              className="btn btn-block submit-btn"
+              onClick={handleSubmit}
+              disabled={isLoading}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
+};
 
 export default AddJob;
